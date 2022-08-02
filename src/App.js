b1cb3479a14669b79bc3652048b598ac0a4eac31
dev <dev@grupocci.local>
2022-08-02 16:42:51 -0300

    Iniciando Projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CCI <code>Turma de Programação Full Stack</code> Ensino Médio.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alô Mundo
         </a>
       </header>
     </div>
